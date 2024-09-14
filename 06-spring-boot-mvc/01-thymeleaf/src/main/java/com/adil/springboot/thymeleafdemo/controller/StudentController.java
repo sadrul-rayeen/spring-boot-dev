7c1fe73179b87a36ad62f1800c0c51fa4a9e0509
Adil <adil@Adils-MacBook-Air.local>
2024-09-14 17:55:46 +0530

    mvc changes

--- 06-spring-boot-mvc/01-thymeleaf/src/main/java/com/adil/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-mvc/01-thymeleaf/src/main/java/com/adil/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.adil.springboot.thymeleafdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,15 @@ import com.adil.springboot.thymeleafdemo.model.Student;
 @Controller
 public class StudentController {
 
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${systems}")
+    private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model thModel) {
 
@@ -19,6 +31,9 @@ public class StudentController {
 
         // add a student object to the model
         thModel.addAttribute("student", theStudent);
+        thModel.addAttribute("countries", countries);
+        thModel.addAttribute("languages", languages);
+        thModel.addAttribute("systems", systems);
 
         return "student-form";
     }
