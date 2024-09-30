70930219a4613c161d336d8d4f325d365fbc4618
Adil <adil@Adils-MacBook-Air.local>
2024-09-30 22:07:53 +0530

    jpa

---
+++ 09-spring-boot-jpa-advanced-mapping/02-jpa-one-to-one-bi/src/test/java/com/adil/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adil.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
